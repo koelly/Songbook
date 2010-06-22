de434732dd06fa2ad6b3b7c168fc36d99ec647ea
koelly <koelly@kampfkoloss.fritz.box>
2010-06-22 10:10:36 +0200

    GPLv3 Header added

--- src/de/koelly/gichtl/ListThem.java
+++ src/de/koelly/gichtl/ListThem.java
@@ -1,5 +1,25 @@
 package de.koelly.gichtl;
 
+/*
+ * Gichtl - Die Harnsäure Datenbank
+ * URLs: http://www.koelly.de/blog/gichtl-app/
+ * Copyright (C) 2010, Christopher Köllmayr
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 import android.app.ListActivity;
 import android.content.Intent;
 import android.database.Cursor;
--- src/de/koelly/gichtl/MySearchableActivity.java
+++ src/de/koelly/gichtl/MySearchableActivity.java
@@ -1,11 +1,30 @@
 package de.koelly.gichtl;
 
+/*
+ * Gichtl - Die Harnsäure Datenbank
+ * URLs: http://www.koelly.de/blog/gichtl-app/
+ * Copyright (C) 2010, Christopher Köllmayr
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 import android.app.ListActivity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
